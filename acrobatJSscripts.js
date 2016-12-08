d250de00a11b7e85927c10ece8909abdbe0e8941
James Pozenel <jamespozenel@quickenloans.com>
2016-12-08 10:31:26 -0500

    1dx string notation added to createDiceChain

--- acrobatJSscripts.js
+++ acrobatJSscripts.js
@@ -8,17 +8,25 @@ Character Portrait: Upload image file functionality
 Totally free! 
 
 Tips welcome.
+
 */
-function createDiceChain(to, from) {
+function createDiceChain(to, from, add1d) {
     // Default parameter values
     to = typeof to !== 'undefined' ? to : 3;
     from = typeof from !== 'undefined' ? from : 30;
+    add1d = typeof add1d !== 'undefined' ? add1d : true;
     // Setting up variables
     var values = [3,4,5,6,7,8,10,12,14,16,20,24,30];
     var diceChain = [];
+    function addString1d(item) {
+      return "1d" + item;
+    }
     // creating return
     if (values.indexOf(to) != -1 && values.indexOf(from) != -1) {
-       diceChain = values.slice(values.indexOf(to), values.indexOf(from)+1);
+      diceChain = values.slice(values.indexOf(to), values.indexOf(from)+1);
+    }
+    if (add1d) {
+      diceChain = diceChain.map(addString1d);
     }
     // return diceChain
     return diceChain;
