25af62742bbe3cff73e3222eda99652eb4fbfbb1
James Pozenel <jamespozenel@quickenloans.com>
2016-12-12 10:42:56 -0500

    Character parent class

---
+++ character.js
@@ -0,0 +1,39 @@
+function Character() = {
+    this.getTitle = function(titles, lvl) {
+        var val = '';
+        if (lvl >=5) {
+            val = titles[4];
+        } else if (lvl < 5) {
+            val = titles[this.lvl-1];
+        }
+        return val;
+    };
+    this.findActionDice = funtion(cClass, lvl) {
+        var threeActions = ['Dwarf', 'Elf', 'Warrior', 'Wizard'];
+        var val ='';
+        if (threeActions.indexOf(cClass) == -1) {
+            if (lvl < 6) {
+                val = "1d20";
+            } else if (lvl == 6) {
+                val = "1d20 + 1d14";
+            } else if (lvl == 7) {
+                val = "1d20 + 1d16";
+            } else {
+                val = "1d20 + 1d20";
+            }
+        } else if (threeActions.indexOf(cClass) > -1) {
+            if (lvl < 5) {
+                val = "1d20";
+            } else if (lvl == 5) {
+                val = "1d20 + 1d14";
+            } else if (lvl == 6) {
+                val = "1d20 + 1d16";
+            } else if (lvl < 10) {
+                val = "1d20 + 1d20";
+            } else {
+                val = "1d20+1d20+1d14";
+            }
+        }
+        return val;
+    };
+}
