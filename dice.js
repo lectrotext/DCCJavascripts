d8010e4a9b3965a8a56c31fdd5ee952669d6c881
James Pozenel <jamespozenel@quickenloans.com>
2016-12-12 12:19:08 -0500

    tighten up

--- dice.js
+++ dice.js
@@ -2,11 +2,11 @@ function Dice() {
     this.stringify = function(die) {
       return "1d" + die;
     };
-    this.chain = function(to, from, add1d) {
+    this.chain = function(to, from, stringify) {
         // Default parameter values
         to = typeof to !== 'undefined' ? to : 3;
         from = typeof from !== 'undefined' ? from : 30;
-        add1d = typeof add1d !== 'undefined' ? add1d : true;
+        stringify = typeof stringify !== 'undefined' ? stringify : true;
         // Setting up variables
         var values = [3,4,5,6,7,8,10,12,14,16,20,24,30];
         var diceChain = [];
@@ -14,7 +14,7 @@ function Dice() {
         if (values.indexOf(to) != -1 && values.indexOf(from) != -1) {
             diceChain = values.slice(values.indexOf(to), values.indexOf(from)+1);
         }
-        if (add1d) {
+        if (stringify) {
             diceChain = diceChain.map(this.stringify);
         }
         // return diceChain
