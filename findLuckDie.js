71559872e810ee3fe0aa413afcd00cc6a053bb53
James Pozenel <jamespozenel@quickenloans.com>
2016-12-12 12:20:50 -0500

    OO architecture makes these irrelevent

--- findLuckDie.js
+++
@@ -1,9 +0,0 @@
-/**
- * Finds the Luck Die for a Thief given his level
- * @param lvl
- * @returns {string}
- */
-function findLuckDie(lvl) {
-    var diceChain = createDiceChain(3,16);
-    return diceChain[lvl - 1];
-}
